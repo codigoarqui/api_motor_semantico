fb466eb58e17498451900dc1b9060bb28a4d5859
codigoarqui <codigoarqui.examples@gmail.com>
2025-08-27 08:09:17 -0500

    cambio modelo

--- app/routes/rag.py
+++ app/routes/rag.py
@@ -27,7 +27,7 @@ async def responder_con_rag(payload: BusquedaRequest):
     RESPUESTA:
     '''
 
-    model = genai.GenerativeModel('gemini-2.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-pro')
     respuesta_generada = model.generate_content(prompt_template)
 
     return {"respuesta": respuesta_generada.text}
